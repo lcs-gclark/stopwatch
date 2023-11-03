24e0ab2bd41f2eb5047c7c5ee5e15899944cf906
grayden clark <gclark@lcs.on.ca>
2023-11-03 12:21:39 -0400

    Initial Commit

---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by Gray Clark on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
