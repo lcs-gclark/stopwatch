a39cf92f58a8728f61a85f0e31cd87f6d1417655
grayden clark <gclark@lcs.on.ca>
2023-11-04 09:54:15 -0400

    first steps

--- stopwatch/ContentView.swift
+++ stopwatch/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //first layer background
+            Color.yellow
+                .ignoresSafeArea()
+            //secondlayer rest of interface
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
